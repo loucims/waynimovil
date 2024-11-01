19784da09acc227834e0aa3ed948d126b209d25e
Sebastian <sebastian.loucim@gmail.com>
2024-11-01 12:36:15 -0300

    Agregado template de typography

--- app/src/main/java/com/ort/waynimovil/ui/theme/Type.kt
+++ app/src/main/java/com/ort/waynimovil/ui/theme/Type.kt
@@ -2,37 +2,33 @@ package com.ort.waynimovil.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import com.ort.waynimovil.R
 
+//Regular es 500
+//Bold es 700
 
-
+val manRopeFamily = FontFamily(
+    Font(R.font.manrope_extralight, FontWeight.ExtraLight),
+    Font(R.font.manrope_light, FontWeight.Light),
+    Font(R.font.manrope_regular, FontWeight.Normal),
+    Font(R.font.manrope_medium, FontWeight.Medium),
+    Font(R.font.manrope_semibold, FontWeight.SemiBold),
+    Font(R.font.manrope_bold, FontWeight.Bold),
+    Font(R.font.manrope_extrabold, FontWeight.ExtraBold)
 )
 
-// Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = TextStyle(
+        fontFamily = manRopeFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 74.sp,  // Convertido directamente de 74px
+        lineHeight = 74.sp, // Convertido directamente de 74px
+        letterSpacing = (-0.02).em // Copiado directamente de la especificación
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    //Agregar todas las demas
 )
