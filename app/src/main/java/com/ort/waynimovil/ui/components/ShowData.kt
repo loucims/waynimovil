054596cf7aa8c46a3bafc91953ac892875c57b73
Sebastian <sebastian.loucim@gmail.com>
2024-11-01 20:34:33 -0300

    agregado materialIcons extended

--- app/src/main/java/com/ort/waynimovil/ui/components/ShowData.kt
+++ app/src/main/java/com/ort/waynimovil/ui/components/ShowData.kt
@@ -2,21 +2,24 @@ package com.ort.waynimovil.ui.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Call
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ort.waynimovil.ui.theme.WaynimovilTheme
 
 
 @Composable
-fun ShowDataButton(onClick: () -> Unit) {
+fun ShowDataButton(isActive: Boolean, onClick: () -> Unit) {
     Row(
         modifier = Modifier
             .clickable { onClick() }
@@ -25,17 +28,20 @@ fun ShowDataButton(onClick: () -> Unit) {
         horizontalArrangement = Arrangement.Center
     ) {
         Icon(
-            imageVector = Icons.Rounded.Call,
+            imageVector = if (isActive) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff,
             contentDescription = "Mostrar datos",
             tint = MaterialTheme.colorScheme.secondary,
             modifier = Modifier
-                .width(16.dp)
-                .height(16.dp)
+                .padding(0.dp, 2.dp, 0.dp, 0.dp)
+                .width(24.dp)
+                .height(24.dp)
                 .padding(start = 1.33333.dp, top = 3.33333.dp, end = 1.33333.dp, bottom = 3.33333.dp)
 
         )
-        Spacer(modifier = Modifier.width(8.dp))
+        Spacer(modifier = Modifier.width(4.dp))
         Text(
+
+            textAlign = TextAlign.Center,
             text = "Mostrar datos",
             style = MaterialTheme.typography.labelMedium,
             color = MaterialTheme.colorScheme.secondary
@@ -48,6 +54,18 @@ fun ShowDataButton(onClick: () -> Unit) {
 fun ShowData(){
     WaynimovilTheme{
         ShowDataButton(
+            isActive = true,
+            onClick = { print("funca")}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HideData(){
+    WaynimovilTheme{
+        ShowDataButton(
+            isActive = false,
             onClick = { print("funca")}
         )
     }
