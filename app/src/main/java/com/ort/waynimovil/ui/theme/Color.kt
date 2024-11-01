0ca371c814695027770e295acbb507a41de6adb6
Tomas-Muras-77 <Tomasmuras04@gmail.com>
2024-11-01 12:13:16 -0300

    feature/colors_n_fonts(wip)

--- app/src/main/java/com/ort/waynimovil/ui/theme/Color.kt
+++ app/src/main/java/com/ort/waynimovil/ui/theme/Color.kt
@@ -1,11 +1,34 @@
 package com.ort.waynimovil.ui.theme
 
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF2A1846)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Gray900 = Color(0xFF575372)
+val Gray500 = Color(0xFFD1D1D7)
+val Gray300 = Color(0xFFEEEEF0)
+val Gray100 = Color(0xFFF8F8F6)
+
+val Green900 = Color(0xFF00A067)
+val Green800 = Color(0xFF0FD08B)
+val Green100 = Color(0xFFCFF6E8)
+
+val Blue900 = Color(0xFF478FF2)
+val Blue500 = Color(0xFF6CA5F5)
+val Blue100 = Color(0xFFDAE9FC)
+
+val Purple800 = Color(0xFF442E83)
+
+val Red900 = Color(0xFFF96440)
+val Red100 = Color(0xFFFEE0D9)
+
+val Yellow900 = Color(0xFFFFCC3E)
+val Yellow800 = Color(0xFFFFE05F)
+val Yellow500 = Color(0xFFFFEC9F)
+val Yellow100 = Color(0xFFFFF9DF)
+
+
+
+
--- app/src/main/java/com/ort/waynimovil/ui/theme/Theme.kt
+++ app/src/main/java/com/ort/waynimovil/ui/theme/Theme.kt
@@ -11,18 +11,27 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+
+
+
+
+
 private val DarkColorScheme = darkColorScheme(
+    /*
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
+    */
+
 )
 
 private val LightColorScheme = lightColorScheme(
+    /*
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
-    /* Other default colors to override
+    Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
