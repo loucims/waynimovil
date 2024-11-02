5bb17a32d89aa08179d771d7623d1a8e24dbf160
Tomas-Muras-77 <Tomasmuras04@gmail.com>
2024-11-02 17:25:17 -0300

    feature/loading_screen

--- app/src/main/java/com/ort/waynimovil/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/ort/waynimovil/ui/screens/SplashScreen.kt
@@ -1,14 +1,27 @@
 package com.ort.waynimovil.ui.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.ort.waynimovil.R
 import com.ort.waynimovil.navigation.Routes
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
+
+    val logo: Painter = painterResource(id = R.drawable.logo_waynimovil)
+
+
     LaunchedEffect(Unit) {
         delay(2000)
         navController.navigate(Routes.LOGIN) {
@@ -16,5 +29,26 @@ fun SplashScreen(navController: NavController) {
         }
     }
 
-    Text(text = "Splash Screen")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.onPrimary),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = logo,
+                contentDescription = "Logo Waynimóvil",
+                tint = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier
+                    .width(257.00244.dp)
+                    .height(36.dp)
+                    .padding(end = 0.00001.dp, bottom = 0.00003.dp)
+
+            )
+
+        }
+    }
 }
