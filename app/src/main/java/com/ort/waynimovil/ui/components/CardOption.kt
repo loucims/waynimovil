18e47af208f516a7e38ee54852db3cfc2552af1f
Sebastian <sebastian.loucim@gmail.com>
2024-11-02 09:47:58 -0300

    renamed TextBar to CardOption

--- app/src/main/java/com/ort/waynimovil/ui/components/CardOption.kt
+++ app/src/main/java/com/ort/waynimovil/ui/components/CardOption.kt
@@ -6,11 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
